05472a51aafec70e598f519fa382ff802a05e0f0
Krzysztof Koch <loczek3603@gmail.com>
2016-09-26 22:55:18 +0100

    Starting work on KrisRTOS, startup file created

---
+++ systemInit.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+* File:     	systemInit.s
+* Brief:    	Initialisation functions for main system components
+* Author: 		Krzysztof Koch
+* Version:		V1.00
+* Date created:	26/09/2016
+* Last mod: 	26/09/2016
+*
+* Note: 		
+*******************************************************************************/
+
+void SystemInit(void) {
+}
---
+++ systeminit.d
@@ -0,0 +1,1 @@
+.\systeminit.o: systemInit.c
